535054caa0f2a3faccb8283597092eb3d122a4f2
Ilumhan <ichaykin@yandex.ru>
2025-05-10 21:15:44 +0300

    2.11 fix after auto-check

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -17,11 +17,11 @@ export function getRandomItem<T>(items: T[]): T {
 }
 
 export function generatePassword() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
+  let length = 8,
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+    retVal = '';
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
   }
   return retVal;
 }
